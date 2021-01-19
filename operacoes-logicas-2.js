317bccc4676b2a1ee6ccc5b8e1ce3b980727b8c9
JadsonStefani <jadsonstefani@gmail.com>
2021-01-19 15:05:25 -0300

    Retirada de comentários

--- operacoes-logicas-2.js
+++ operacoes-logicas-2.js
@@ -1,25 +1,3 @@
-/*
- passou:= falso
-   escreval("Digite o nome do Aluno: ")
-   leia(nome)
-   escreval("Digite a nota 1 do aluno: ")
-   leia(nota1)
-   escreval("Digite a nota 2 do aluno: ")
-   leia(nota2)
-
-   media := (nota1 + nota2) / 2
-
-   Se media >= 5 entao
-     passou := verdadeiro
-   fimse
-
-   Se ((passou) e ((media >= 5) ou (media < 7))) entao
-      escreval("Aprovado.")
-   senao
-        escreval("Reprovado.")
-   fimse
-*/
-
 var nome, nota1, nota2, media, passou;
 
 passou = false;
